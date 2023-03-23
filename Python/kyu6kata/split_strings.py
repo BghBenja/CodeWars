96aa9d1ca2be8df3689a8d115d88bc88f47f263a
BghBenja <bghbenja@gmail.com>
2023-03-23 18:15:00 +0100

    Solution Python Split Strings

---
+++ Python/kyu6kata/split_strings.py
@@ -0,0 +1,18 @@
+"""
+Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+Examples:
+
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef']
+"""
+
+def mine_solution(s):
+    if len(s) == 0:
+        return []
+    result = [s[i:i+2] for i in range(0, len(s), 2)]
+
+    if len(result[-1]) == 1:
+        result[-1] += "_"
+
+    return result

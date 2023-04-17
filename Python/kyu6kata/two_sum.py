eeb994a06d6987ec5eb446c0a72827591b34da7c
BghBenja <bghbenja@gmail.com>
2023-04-17 12:17:28 +0200

    Solution Python Tow Sum

---
+++ Python/kyu6kata/two_sum.py
@@ -0,0 +1,11 @@
+def mine_two_sum(numbers, target):
+    for i in range(len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            if numbers[i] + numbers[j] == target:
+                return [i, j]
+            
+def best_two_sum(nums, t):
+    for i, x in enumerate(nums):
+        for j, y in enumerate(nums):
+            if i != j and x + y == t:
+                return [i, j]
